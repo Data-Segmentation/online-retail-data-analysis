77b87cc0b264a485ed7a6eaa6c89bb321c4baff2
Owusu Amaning Kwarteng <amaning.kwarteng08@gmail.com>
2024-07-25 22:00:05 +0000

    Update valuable-items.py

--- scripts/EDA/valuable-items.py
+++ scripts/EDA/valuable-items.py
@@ -7,7 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # load data
-df = pd.read_csv('../../data/processed/cleaned-data.csv')
+df = pd.read_csv('../../data/cleaned-data.csv')
 
 # items that yield high returns (first ten)
 valuable_items = df.groupby('Description')['TotalPrice'].sum().nlargest(10)
