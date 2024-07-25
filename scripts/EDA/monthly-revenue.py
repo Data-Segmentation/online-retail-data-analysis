20685f04b5e13f27c5022eac3c357e25596d47d4
Owusu Amaning Kwarteng <amaning.kwarteng08@gmail.com>
2024-07-25 21:58:17 +0000

    Update monthly-revenue.py

--- scripts/EDA/monthly-revenue.py
+++ scripts/EDA/monthly-revenue.py
@@ -7,7 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # load data
-df = pd.read_csv('../../data/processed/cleaned-data.csv')
+df = pd.read_csv('../../data/cleaned-data.csv')
 
 # monthly total revenue
 monthly_revenue = df.groupby('InvoiceMonth')['TotalPrice'].sum()
