b428140c4e175b20cfd1037fc75a38be90ec1457
Owusu Amaning Kwarteng <amaning.kwarteng08@gmail.com>
2024-07-25 21:59:09 +0000

    Update scatter-plot.py

--- scripts/EDA/scatter-plot.py
+++ scripts/EDA/scatter-plot.py
@@ -7,7 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # load data
-df = pd.read_csv('../../data/processed/cleaned-data.csv')
+df = pd.read_csv('../../data/cleaned-data.csv')
 
 # correlation between item quantities and unit price
 import seaborn as sns
